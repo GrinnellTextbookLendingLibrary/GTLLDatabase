efbb4fd3a9325856c5626558454d56008173059b
Tooplark <tooplark@hotmail.com>
2012-10-14 09:56:20 -0500

    Added Book model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtll::Application.config.secret_token = 'b3757891d5703ace2a76887e2deeb4901daf1d1a47a05c6726a4b6142547db5ddf729cd01883434a67b41a66b226eaf8344a256034a7999421628cc455c425fc'
