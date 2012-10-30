c09c1af3eaff2ab09d0ba4f7e26fc194c483b7e2
Kate Ingersoll <katelingersoll@gmail.com>
2012-10-30 15:16:56 -0500

    added tests for uniqueness

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -19,6 +19,21 @@ describe Book do
     no_authors_book = Book.new(@attr.merge(:authors => ""))
     no_authors_book.should_not be_valid
   end
-  
+
+  it "should not allow duplicate book" do
+    #Same title, author, edition, and edition is present
+    Book.create!(@attr)
+    book_with_duplicate_attributes = Book.new(@attr)
+    book_with_duplicate_attributes.should_not be_valid
+  end
+
+  it "should not allow duplicate book without edition label" do
+    #Same title, author, edition, with edition being null
+    Book.create!(:name => "Lovely", :authors => "Splendid")
+    book_with_duplicate_attributes = Book.new(:name => "Lovely", 
+                                              :authors => "Splendid")
+    book_with_duplicate_attributes.should_not be_valid
+  end
+ 
 
 end
